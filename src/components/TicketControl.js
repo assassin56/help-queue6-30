9907034cddb1e9fcb1a9fcf833829b206034952c
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-30 14:03:48 -0700

    clean up if branch for the click handler.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -14,53 +14,65 @@ class TicketControl extends React.Component {
     };
 
   }
-  handleClick = () => {
-    let checkQuestion;
+  handleClick = ()=>{
     if (!this.state.formVisibleOnPage) {
       switch (this.state.checkQuestion) {
         case (0):
-          this.setState(prevState => ({ checkQuestion: 1 }))
-          console.log(this.state.checkQuestion)
+          this.setState({  checkQuestion:1 })
           break;
         case (1):
-          this.setState(prevState => ({ checkQuestion: 2 }))
+          this.setState({  checkQuestion:2 })
           break;
         case (2):
-          this.setState({ checkQuestion: 3, formVisibleOnPage: true })
+          this.setState({  checkQuestion:3 , formVisibleOnPage: true})
+          break;
+        case (3):
+            this.setState({  checkQuestion:3 , formVisibleOnPage: true})
+        break;
+        default:
+          this.setState({  checkQuestion:4 })
           break;
       }
     }
-    this.setState({ checkQuestion });
+   // this.setState({ checkQuestion });
   }
 
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    let questionText = ["Have you read through LHTP?", "have you asked another pair?", "have you prayed?"]
+    let questionText = ["Have you read through LHTP?", "have you asked another pair?", "have you prayed?", ""]
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />;
+      questionText = questionText[this.state.checkQuestion]
+        console.log(questionText)
       buttonText = "Return to Ticket List";
     } else {
-      if (this.state.checkQuestion == 0) {
+      if (this.state.checkQuestion === 0) {
         currentlyVisibleState = <Check1 />;
         buttonText = "Yes";
-      } else if (this.state.checkQuestion == 1) {
+      } else if (this.state.checkQuestion === 1) {
         currentlyVisibleState = <Check1 />;
         buttonText = "Yes";
-        questionText = questionText[Check1.checkQuestion]
-      } else if (this.state.checkQuestion == 2) {
+        questionText = questionText[this.state.checkQuestion]
+      } else if (this.state.checkQuestion === 2) {
         currentlyVisibleState = <Check1 />;
+        questionText = questionText[this.state.checkQuestion]
         buttonText = "Yes";
-      } else if (this.state.checkQuestion == 3) {
+      } else if (this.state.checkQuestion === 3) {
         currentlyVisibleState = <Check1 />
+        questionText = questionText[this.state.checkQuestion]
+        console.log(questionText)
         buttonText = "Yes";
+      }else {
+      currentlyVisibleState = <TicketList/>
       }
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        {questionText[Check1.checkQuestion]}
-        <button onClick={this.handleClick}>{buttonText}</button> {}
+        {typeof questionText !== "string" ? questionText[0]: questionText}
+        <button onClick={this.handleClick}>{buttonText}</button> 
+        
       </React.Fragment>
     );
   }
